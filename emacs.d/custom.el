d784e8b78f266e5f23fb997e7760964a53e072ef
Øyvind Grønnesby <oyving@pvv.org>
2012-07-27 10:50:59 +0200

    Use customize to select a theme

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,1 +1,13 @@
 ;; placeholder file for customized variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
